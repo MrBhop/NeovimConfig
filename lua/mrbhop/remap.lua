f72798f85f6ddd098761acde4d2a56bbf2890af7
Florian Schuett <flo.schuett@chello.at>
2025-05-23 22:46:24 +0200

    Add sets and remaps

--- lua/mrbhop/remap.lua
+++ lua/mrbhop/remap.lua
@@ -1,4 +1,41 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- open netrw
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- move selection
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- keep the cursor centered when jumping
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- yank to clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- delete into void register
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+-- with paste
+vim.keymap.set("v", "<leader>p", "\"_dP")
+
+-- Quickfix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- replace current word
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+
+-- toggle word-wrap
+vim.keymap.set("n", "<leader>tw", function() vim.opt.wrap = not vim.opt.wrap:get() end)
+-- toggle highlight search
+vim.keymap.set("n", "<leader>th", function() vim.opt.hlsearch = not vim.opt.hlsearch:get() end)
