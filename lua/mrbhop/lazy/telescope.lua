9af59f3a26e375a77af75ea9db68084bdaf3fc84
Florian Schuett <flo.schuett@chello.at>
2025-06-25 21:50:59 +0200

    Modify telescope config

--- lua/mrbhop/lazy/telescope.lua
+++ lua/mrbhop/lazy/telescope.lua
@@ -8,28 +8,49 @@ return {
 		require('telescope').setup({})
 
 		local builtin = require('telescope.builtin')
+
+		-- project files
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+
+		-- project ignored files
+		vim.keymap.set('n', '<leader>pif', function()
+			builtin.find_files({
+				no_ignore = true,
+			})
+		end)
+
+		-- git files
 		vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+
 		-- project word search
 		vim.keymap.set('n', '<leader>pws', function()
 			local word = vim.fn.expand("<cword>")
 			builtin.grep_string({ search = word })
 		end)
-		-- Project WORD search
+		-- project WORD search
 		vim.keymap.set('n', '<leader>pWs', function()
 			local word = vim.fn.expand("<cWORD>")
 			builtin.grep_string({ search = word })
 		end)
+
 		-- Grep search
 		vim.keymap.set('n', '<leader>ps', function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
-		-- Vim help
-		vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-				-- Live Grep
-		vim.keymap.set('n', '<leader>pls', function()
-			builtin.live_grep()
+			builtin.grep_string({
+				search = vim.fn.input("Grep > "),
+				use_regex = true,
+			})
 		end)
 
+		-- vim help
+		vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+		-- Project live search
+		vim.keymap.set('n', '<leader>pls', builtin.live_grep)
+
+		-- buffer live search
+		vim.keymap.set('n', '<leader>bls', builtin.current_buffer_fuzzy_find)
+
+		-- list cmd
+		vim.keymap.set('n', '<leader>lcmd', builtin.commands)
 	end
 }
