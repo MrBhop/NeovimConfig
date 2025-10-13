752cad53030f849840a5e6bdc8ff5ec5fcb4e6ee
Florian Schuett <flo.schuett@chello.at>
2025-10-13 14:16:32 +0200

    Add lazydev and clean up lsp setup

--- lua/mrbhop/lazy/lsp.lua
+++ lua/mrbhop/lazy/lsp.lua
@@ -1,5 +1,18 @@
 return {
 	"neovim/nvim-lspconfig",
+	dependencies = {
+		{
+			"folke/lazydev.nvim",
+			ft = "lua", -- only load on lua files
+			opts = {
+				library = {
+					-- See the configuration section for more details
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+	},
 	config = function()
 		vim.lsp.config("*", {
 			on_attach = function(_, bufnr)
@@ -9,40 +22,20 @@ return {
 				vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 				vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 				vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-				vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.open_float() end, opts)
+				vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
 				vim.keymap.set("n", "[d", function() vim.lsp.buf.goto_next() end, opts)
 				vim.keymap.set("n", "]d", function() vim.lsp.buf.goto_prev() end, opts)
-				vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-				vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-				vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-				vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 			end
 		})
 
-		vim.lsp.config("lua_ls", {
-			cmd = { "lua-language-server" },
-			filetypes = { "lua" },
-			root_markers = { ".luarc.json", ".git" },
+		vim.lsp.config("basedpyright", {
 			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						checkThirdParty = false,
-						library = {
-							vim.env.VIMRUNTIME,
-							vim.fn.stdpath("config") .. "/lua",
-						},
-					},
-				},
+				locale = "en",
 			},
 		})
 
 		vim.diagnostic.config({
 			virtual_text = true,
 		})
-
-		vim.lsp.enable("lua_ls")
 	end,
 }
